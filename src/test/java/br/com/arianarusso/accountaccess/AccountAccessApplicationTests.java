c102ae6bf8ce90dc7b73b3faa804ce9782f5e713
ArianaRusso <1214Unicamp!>
2023-11-08 09:32:25 -0300

    commit inicial

---
+++ src/test/java/br/com/arianarusso/accountaccess/AccountAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arianarusso.accountaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
